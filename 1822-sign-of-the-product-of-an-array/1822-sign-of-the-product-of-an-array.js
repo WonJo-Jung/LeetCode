94f4bd93ebaa1cdcb90ba7b28221a407935cea57
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-02 10:48:00 +0900

    Time: 52 ms (92.56%), Space: 44.1 MB (30.62%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var arraySign = function(nums) {
-    const counts = nums.filter(n => n<0).length;
     if(nums.some(n => n==0)) return 0;
-    else if(counts % 2 == 0) return 1;
+    const counts = nums.filter(n => n<0).length;
+    if(counts % 2 == 0) return 1;
     else return -1;
 };
