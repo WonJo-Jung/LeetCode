30b738e68a194261a07eeb57340de0fc914ad265
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-02 11:30:24 +0900

    Time: 57 ms (68.00%), Space: 41.8 MB (88.30%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.js
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -3,13 +3,8 @@
  * @return {number}
  */
 var maximumWealth = function(accounts) {
-    let answer = -1;
-    for(let i=0; i<accounts.length; i++) {
-      let sum = 0;
-      for(let j=0; j<accounts[i].length; j++) {
-        sum += accounts[i][j];
-      }
-      if(answer < sum) answer = sum;
-    }
-    return answer;
+  for(let i=0; i<accounts.length; i++) {
+    accounts[i] = accounts[i].reduce((acc, curr) => acc + curr, 0);
+  }
+  return Math.max.apply(null, accounts);
 };
