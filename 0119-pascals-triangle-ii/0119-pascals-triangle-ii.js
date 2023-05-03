d1a430d700dca6bff1d62c5e5a62a9be28ab32ff
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-03 14:09:42 +0900

    Time: 55 ms (76.80%), Space: 42.3 MB (29.96%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -5,11 +5,9 @@
 var getRow = function(rowIndex) {
   let answer = [1];
   for(let i=1; i<=rowIndex; i++) {
-    let cal1 = JSON.parse(JSON.stringify(answer));
-    cal1.push(0);
-    let cal2 = JSON.parse(JSON.stringify(answer));
-    cal2.unshift(0);
-    answer = cal1.map((v,i) => v+cal2[i]);
+    answer = answer.map((v,i) => v+answer[i+1]);
+    answer.unshift(1);
+    answer[answer.length-1] = 1;
   }
   return answer;
 };
