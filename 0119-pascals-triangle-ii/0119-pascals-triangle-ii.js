d4edf1d943a2a72091ab7547cf5b8ea9e35ea453
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-03 16:18:48 +0900

    Time: 57 ms (69.07%), Space: 42.2 MB (44.82%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -3,20 +3,14 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-  // let answer = [1];
-  // for(let i=1; i<=rowIndex; i++) {
-  //   answer.push(0);
-  //   let prev = [answer[0]];
-  //   for(let j=1; j<answer.length; j++) {
-  //     prev.push(answer[j]);
-  //     answer[j] += prev.shift();
-  //   }
-  // }
-  // return answer;
-  
-  var ans = new Array(rowIndex+1)
-  ans[0]=ans[rowIndex]=1
-  for(let i=1,up=rowIndex;i<rowIndex;i++,up--)
-      ans[i] = ans[i-1]*up/i
-  return ans
+  let answer = [1];
+  for(let i=1; i<=rowIndex; i++) {
+    answer.push(0);
+    let prev = [answer[0]];
+    for(let j=1; j<answer.length; j++) {
+      prev.push(answer[j]);
+      answer[j] += prev.shift();
+    }
+  }
+  return answer;
 };
