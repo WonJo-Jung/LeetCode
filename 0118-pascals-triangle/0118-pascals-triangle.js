dc215400f0108d33f0b5e2fe087d9e620d877a2c
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-03 12:16:01 +0900

    Time: 69 ms (14.04%), Space: 43.7 MB (8.30%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+  let answer = [];
+  answer.push([1]);
+  if(numRows == 1) return answer;
+  answer.push([1,1]);
+  if(numRows == 2) return answer;
+  for(let i=2; i<numRows; i++) {
+    let calculation = answer[i-1].map((v, j) => v+answer[i-1][j+1]);
+    calculation.pop();
+    calculation.unshift(1);
+    calculation.push(1);
+    answer.push(calculation);
+  }
+  return answer;
+};
