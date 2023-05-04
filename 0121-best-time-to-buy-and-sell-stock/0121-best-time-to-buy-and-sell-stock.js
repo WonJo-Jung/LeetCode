6f4fddca31e155877c2b1b9632c17abc7c7fd8a0
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-04 09:40:14 +0900

    Time: 70 ms (96.27%), Space: 51.8 MB (47.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,24 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let max_profit = 0;
-  for(let left=0, right=1; right<prices.length; right++) {
-    const diff = prices[right] - prices[left];
-    if(diff < 0) {
-      left = right;
-    } else {
-      if(diff > max_profit) {
-        max_profit = prices[right] - prices[left];  
-      }
-    }
+  // let max_profit = 0;
+  // for(let left=0, right=1; right<prices.length; right++) {
+  //   const diff = prices[right] - prices[left];
+  //   if(diff < 0) {
+  //     left = right;
+  //   } else {
+  //     if(diff > max_profit) {
+  //       max_profit = prices[right] - prices[left];  
+  //     }
+  //   }
+  // }
+  // return max_profit;
+  
+  let profit = 0;
+  let buy = Infinity;
+  for(let i=0; i<prices.length; i++) {
+    buy = Math.min(buy, prices[i]);
+    profit = Math.max(profit, prices[i] - buy);
   }
-  return max_profit;
+  return profit;
 };
