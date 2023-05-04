191bf05fdf051eb9b47bed31207023f64d8c23da
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-04 10:14:24 +0900

    Time: 69 ms (97.10%), Space: 52 MB (23.73%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -18,23 +18,24 @@ var maxProfit = function(prices) {
   // return max_profit;
   
   /* best approach */
-  // let profit = 0;
-  // let buy = Infinity;
-  // for(let i=0; i<prices.length; i++) {
-  //   buy = Math.min(buy, prices[i]);
-  //   profit = Math.max(profit, prices[i] - buy);
-  // }
-  // return profit;
-  
-  const n = prices.length;
-  let dp = new Array(n);
-  dp[0] = new Array(2);
-  dp[0][0] = 0; // max profit
-  dp[0][1] = -prices[0]; // least price
-  for(let i=1; i<n; i++) {
-    dp[i] = new Array(2);
-    dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
-    dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
+  let profit = 0;
+  let buy = Infinity;
+  for(let i=0; i<prices.length; i++) {
+    buy = Math.min(buy, prices[i]);
+    profit = Math.max(profit, prices[i] - buy);
   }
-  return dp[n-1][0];
+  return profit;
+  
+  /* dp approach */
+  // const n = prices.length;
+  // let dp = new Array(n);
+  // dp[0] = new Array(2);
+  // dp[0][0] = 0; // max profit
+  // dp[0][1] = -prices[0]; // least price
+  // for(let i=1; i<n; i++) {
+  //   dp[i] = new Array(2);
+  //   dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+  //   dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
+  // }
+  // return dp[n-1][0];
 };
