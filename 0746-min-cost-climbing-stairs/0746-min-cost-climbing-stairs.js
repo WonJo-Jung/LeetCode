a22d1c01787d5324c0f3e76afeaf117505145e46
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-05 20:57:11 +0900

    Time: 69 ms (34.71%), Space: 42 MB (96.26%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -4,10 +4,19 @@
  */
 var minCostClimbingStairs = function(cost) {
   const n = cost.length;
-  let dp = new Array(n+2);
-  dp[n] = dp[n+1] = 0;
-  for(let i=n-1; i>=0; i--) {
-    dp[i] = cost[i] + Math.min(dp[i+1], dp[i+2]);
+  // let dp = new Array(n+2);
+  // dp[n] = dp[n+1] = 0;
+  // for(let i=n-1; i>=0; i--) {
+  //   dp[i] = cost[i] + Math.min(dp[i+1], dp[i+2]);
+  // }
+  // return Math.min(dp[0], dp[1]);
+  
+  let first = cost[0], second = cost[1];
+  for(let i=2; i<n; i++) {
+    let prev = first < second ? first : second;
+    let current = cost[i] + prev;
+    first = second;
+    second = current;
   }
-  return Math.min(dp[0], dp[1]);
+  return first < second ? first : second;
 };
