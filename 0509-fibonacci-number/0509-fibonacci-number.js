be1d1c2dcd50c9afc22ef05633709a180134719c
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-05 15:27:59 +0900

    Time: 58 ms (74.36%), Space: 41.3 MB (91.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -5,11 +5,10 @@
 var fib = function(n) {
   if(n===0) return 0;
   if(n===1) return 1;
-  let prev = new Array(2), num = 2;
-  prev[0] = 0, prev[1] = 1;
+  let prev1 = 0, prev2 = 1, num = 2;
   while(num++ < n) {
-    if(num%2==1) prev[0] += prev[1];
-    else prev[1] += prev[0];
+    if(num%2==1) prev1 += prev2;
+    else prev2 += prev1;
   }
-  return prev[0] + prev[1];
+  return prev1 + prev2;
 };
