60b05b87032b38e1f3e3c05bc06ceefe2d4c84dd
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-05 13:48:16 +0900

    Time: 94 ms (45.25%), Space: 49.4 MB (43.35%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -3,11 +3,24 @@
  * @return {number[]}
  */
 var countBits = function(n) {
-  let answer = [0,1,1,2], base = 3, increment = 2;
-  while(base < n) {
-    let next = JSON.parse(JSON.stringify(answer)).map(v => v+1);
-    answer = answer.concat(next);
-    base += increment *= 2;
+  // let answer = [0,1,1,2], base = 3, increment = 2;
+  // while(base < n) {
+  //   let next = JSON.parse(JSON.stringify(answer)).map(v => v+1);
+  //   answer = answer.concat(next);
+  //   base += increment *= 2;
+  // }
+  // return answer.slice(0, n+1);
+
+  let answer = [0], index = 0, base = 1, increment = 1;
+  for(let num=1; num<=n; num++) {
+    answer.push(answer[index]+1);
+    if(num == base) {
+      index = 0;
+      base += increment *= 2;
+    }
+    else {
+      index += 1;
+    }
   }
-  return answer.slice(0, n+1);
+  return answer;
 };
