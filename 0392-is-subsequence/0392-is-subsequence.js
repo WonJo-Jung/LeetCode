cba85760f22da9d0629af565eb93091264ec4e4b
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-05 14:51:20 +0900

    Time: 49 ms (96.50%), Space: 42.1 MB (45.70%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -4,21 +4,11 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-  // while(s.length && t.indexOf(s[0]) != -1) {
-  //   let pos = t.indexOf(s[0]);
-  //   s = s.slice(1, s.length);
-  //   t = t.slice(pos+1, t.length);
-  // }
-  // if(!s.length) return true;
-  // else return false;
-  
-  
-    let i = 0, j = 0;
-    while( j < t.length) {
-        if(s[i] === t[j]) {
-            i++;
-        }
-        j++;        
-    }
-    return i === s.length ? true: false;
+  while(s.length && t.indexOf(s[0]) != -1) {
+    let pos = t.indexOf(s[0]);
+    s = s.slice(1, s.length);
+    t = t.slice(pos+1, t.length);
+  }
+  if(!s.length) return true;
+  else return false;
 };
