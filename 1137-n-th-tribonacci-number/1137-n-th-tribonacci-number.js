57cf4ea7b6e0e64ec7561d916e6741bc1b54a24a
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-06 14:33:10 +0900

    Time: 52 ms (85.39%), Space: 42 MB (26.81%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -3,19 +3,20 @@
  * @return {number}
  */
 var tribonacci = function(n) {
-  let left = 0, medium = 1, right = 1;
   if(n <= 2) {
     if(n <= 1) {
       if(n <= 0) {
-        return left;
-      } else return medium;
-    } else return right;
+        return 0;
+      } else return 1;
+    } else return 1;
+  } else {
+    let left = 0, medium = 1, right = 1;
+    while(n-- > 2) {
+      let curr = left + medium + right;
+      left = medium;
+      medium = right;
+      right = curr;
+    }
+    return right;
   }
-  while(n-- > 2) {
-    let curr = left + medium + right;
-    left = medium;
-    medium = right;
-    right = curr;
-  }
-  return right;
 };
