bd1ef1e0de96deac1f13aee35ae1f294584246e8
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-06 14:03:21 +0900

    Time: 66 ms (17.65%), Space: 41.4 MB (90.76%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.js
+++ 1025-divisor-game/1025-divisor-game.js
@@ -3,14 +3,16 @@
  * @return {boolean}
  */
 var divisorGame = function(n) {
-  let player = 0;
-  while(n > 1) {
-    let x = 1;
-    n -= x;
-    player = (player+1) % 2;
-  }
-  if(player == 0) return false;
-  else if(player == 1) return true;
+  // let player = 0;
+  // while(n > 1) {
+  //   let x = getMaxFactor(n);
+  //   n -= x;
+  //   player = (player+1) % 2;
+  // }
+  // if(player == 0) return false;
+  // else if(player == 1) return true;
+  let answer = n%2 == 0 ? true : false;
+  return answer;
 };
 
 // let getMaxFactor = function(n) {
