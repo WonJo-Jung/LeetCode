04b82651f6dc61feab8c1060a48a99964cf74259
Jung Won Jo <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-06 14:53:06 +0900

    Time: 56 ms (76.52%), Space: 41.6 MB (87.12%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.js
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.js
@@ -3,14 +3,12 @@
  * @return {number}
  */
 var getMaximumGenerated = function(n) {
-  let nums = [0,1];
-  if(n === 0) return nums[0];
-  else if(n === 1) return nums[1];
-  let max = nums[1];
+  if(n <= 1) return n;
+  let nums = new Array(n+1);
+  nums[0] = 0, nums[1] = 1;
   for(let i=2; i<=n; i++) {
     if(i % 2 === 0) nums[i] = nums[i/2];
     else nums[i] = nums[(i-1)/2] + nums[(i-1)/2+1];
-    max = max < nums[i] ? nums[i] : max;
   }
-  return max;
+  return Math.max.apply(null, nums);
 };
