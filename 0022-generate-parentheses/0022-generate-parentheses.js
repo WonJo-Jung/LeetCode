3f18276f31d2791086815deb2978ea2faf68033a
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-07 18:47:16 +0900

    Time: 60 ms (64.20%), Space: 41.8 MB (99.20%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -3,23 +3,38 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-  let output = ["()"];
+  let output = [[""],["()"]];
   let num = 2;
-  while(num++ <= n) {
-    let len = output.length;
-    while(len--) {
-      let parentheses = output.shift();
-      let count = parentheses.length;
-      for(let i=0; i<=count; i++) {
-        for(let j=i+1; j<count+2; j++) {
-          let new_parentheses = "";
-          new_parentheses += parentheses.slice(0, i) + "(";
-          new_parentheses += parentheses.slice(i, j-1) + ")" + parentheses.slice(j-1, count);
-          output.push(new_parentheses);
+  while(num <= n) {
+    let arr = [];
+    for(let count=0; count<num; count++) {
+      for(let i=0; i<output[count].length; i++) { // f(0), f(1), f(2)
+        for(let j=0; j<output[num-1-count].length; j++) { // f(2), f(1), f(0)
+          arr.push("("+ output[count][i] +")" + output[num-1-count][j]);
         }
       }
-      output = Array.from(new Set([...output]));
     }
+    output[num++] = arr;
   }
-  return output;
+  return output[n];
+  
+  // let output = ["()"];
+  // let num = 2;
+  // while(num++ <= n) {
+  //   let len = output.length;
+  //   while(len--) {
+  //     let parentheses = output.shift();
+  //     let count = parentheses.length;
+  //     for(let i=0; i<=count; i++) {
+  //       for(let j=i+1; j<count+2; j++) {
+  //         let new_parentheses = "";
+  //         new_parentheses += parentheses.slice(0, i) + "(";
+  //         new_parentheses += parentheses.slice(i, j-1) + ")" + parentheses.slice(j-1, count);
+  //         output.push(new_parentheses);
+  //       }
+  //     }
+  //     output = Array.from(new Set([...output]));
+  //   }
+  // }
+  // return output;
 };
