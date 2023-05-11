47a28f5e30a9e403c8efc70678c05c317c756501
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-11 09:31:02 +0900

    Time: 57 ms (92.23%), Space: 44.3 MB (50.29%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -11,7 +11,7 @@ var minPathSum = function(grid) {
   for(let i=1; i<m; i++) {
     grid[i][0] += grid[i-1][0];
     for(let j=1; j<n; j++) {
-      grid[i][j] += grid[i][j-1] > grid[i-1][j] ? grid[i-1][j] : grid[i][j-1];
+      grid[i][j] += Math.min(grid[i][j-1], grid[i-1][j]);
     }
   }
   return grid[m-1][n-1];
