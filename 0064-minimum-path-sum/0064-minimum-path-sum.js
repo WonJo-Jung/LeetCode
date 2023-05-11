da82357eca54fa1ba034aa39f770260cd7877c7a
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-11 09:18:41 +0900

    Time: 66 ms (63.95%), Space: 44.8 MB (34.39%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -5,16 +5,25 @@
 var minPathSum = function(grid) {
   const m = grid.length;
   const n = grid[0].length;
-  let array = new Array(n);
-  array[0] = grid[0][0];
-  for(let i=1; i<n; i++) {
-    array[i] = grid[0][i] + array[i-1];
-  }
-  for(let i=1; i<m; i++) {
-    array[0] += grid[i][0];
+  for(let i=0; i<m; i++) {
+    if(i > 0) grid[i][0] += grid[i-1][0];
     for(let j=1; j<n; j++) {
-      array[j] = grid[i][j] + Math.min(array[j-1], array[j]);
+      if(i == 0) grid[0][j] += grid[0][j-1];
+      else grid[i][j] += Math.min(grid[i][j-1], grid[i-1][j]);
     }
   }
-  return array[n-1];
+  return grid[m-1][n-1];
+
+  // let array = new Array(n);
+  // array[0] = grid[0][0];
+  // for(let i=1; i<n; i++) {
+  //   array[i] = grid[0][i] + array[i-1];
+  // }
+  // for(let i=1; i<m; i++) {
+  //   array[0] += grid[i][0];
+  //   for(let j=1; j<n; j++) {
+  //     array[j] = grid[i][j] + Math.min(array[j-1], array[j]);
+  //   }
+  // }
+  // return array[n-1];
 };
