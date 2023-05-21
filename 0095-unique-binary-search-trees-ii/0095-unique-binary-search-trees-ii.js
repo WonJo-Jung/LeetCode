7510eaf612c1124afb1e1c52eb070b08c0879b3a
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-21 23:33:27 +0900

    Time: 85 ms (65.36%), Space: 48.4 MB (56.98%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.js
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.js
@@ -10,13 +10,42 @@
  * @param {number} n
  * @return {TreeNode[]}
  */
-var generateTrees = function(n, l = 1, r = n, res = []) {
-  for(let i = l; i <= r; i++){
-    for(const left of generateTrees(n, l, i - 1)){
-      for(const right of generateTrees(n, i + 1, r)){
-          res.push(new TreeNode(i, left, right));
+// var generateTrees = function(n, l = 1, r = n, res = []) {
+var generateTrees = function(n) {
+  let bsts = new Array(n+1);
+  bsts[0] = [null], bsts[1] = [{val:1, left:null, right:null}];
+  for(let i=2; i<=n; i++) {
+    bsts[i] = [];
+    for(let j=1; j<=i; j++) {
+      for(const nodeL of bsts[j-1]) {
+        for(const nodeR of bsts[i-j]) {
+          let node = {val:j};
+          node.left = nodeL;
+          node.right = clone(nodeR, j);
+          bsts[i][bsts[i].length] = node;
+        }
       }
     }
   }
-  return n ? res.length ? res : [null] : [];
+  return bsts[n];
+  // for(let i = l; i <= r; i++){
+  //   for(const left of generateTrees(n, l, i - 1)){
+  //     for(const right of generateTrees(n, i + 1, r)){
+  //         res.push(new TreeNode(i, left, right));
+  //     }
+  //   }
+  // }
+  // return n ? res.length ? res : [null] : [];
 };
+
+/**
+ * @param {TreeNode} node 
+ * @param {number} offset 
+ */
+let clone = function(node, offset) {
+  if(node === null) return null;
+  let newNode = {val:node.val + offset};
+  newNode.left = clone(node.left, offset);
+  newNode.right = clone(node.right, offset);
+  return newNode;
+}
