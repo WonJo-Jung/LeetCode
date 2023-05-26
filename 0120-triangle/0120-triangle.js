9d8cf27517ba8c2173d8c3bfb5e5932b1ae662b1
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-27 07:11:43 +0900

    Time: 71 ms (27.43%), Space: 42.9 MB (50.63%) - LeetHub

--- 0120-triangle/0120-triangle.js
+++ 0120-triangle/0120-triangle.js
@@ -7,11 +7,8 @@ var minimumTotal = function(triangle) {
   let dp = new Array(n).fill(0); // f(0)
   dp[0] = triangle[0][0]; // f(1)
   for(let i=2; i<=n; i++) { // f(2)
-    triangle[i-2].unshift(Infinity);
-    triangle[i-2].push(Infinity);
     for(let j=0; j<i; j++) {
-      dp[j] = triangle[i-1][j] + Math.min(triangle[i-2][j], triangle[i-2][j+1]);
-      // dp[j] = triangle[i-1][j] + Math.min(j-1<0 ? Infinity : triangle[i-2][j-1], j+1 === i ? Infinity : triangle[i-2][j]);
+      dp[j] = triangle[i-1][j] + Math.min(j-1<0 ? Infinity : triangle[i-2][j-1], j+1 === i ? Infinity : triangle[i-2][j]);
     }
     for(let j=0; j<i; j++) {
       triangle[i-1][j] = dp[j];
