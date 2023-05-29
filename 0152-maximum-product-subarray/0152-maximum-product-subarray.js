abc3bb915a6d772915982566751223692c7325ee
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-29 11:09:10 +0900

    Time: 57 ms (87.46%), Space: 42.5 MB (56.91%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,14 +3,18 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-  let max = nums[0];
+  let max = nums[0], min = nums[0], result = nums[0];
   for(let i=1; i<nums.length; i++) {
-    let product = nums[i];
-    if(max < product) max = product;
-    for(let j=i-1; j>=0; j--) {
-      product *= nums[j];
-      if(max < product) max = product;
-    }
+    let product = max;
+    max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+    min = Math.min(Math.min(product * nums[i], min * nums[i]), nums[i]);
+    if(result < max) result = max;
+    // if(max < product) max = product;
+    // for(let j=i-1; j>=0; j--) {
+    //   product *= nums[j];
+    //   if(max < product) max = product;
+    // }
   }
-  return max;
+  return result;
+  // return max;
 };
