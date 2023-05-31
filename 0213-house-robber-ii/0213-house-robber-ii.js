31aa8e1031e5724ebad0de6de6babf0141d549e6
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-31 16:55:02 +0900

    Time: 85 ms (5.32%), Space: 48.5 MB (5.16%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  if(nums.length === 1) return nums[0];
+  else if(nums.length <= 3) return Math.max.apply(null, nums);
+  let dp = [], s, e;
+  for(let i=0; i<nums.length; i++) {
+    s = i+2, e = i-2;
+    if(s > nums.length) s -= nums.length;
+    if(e < 0) e += nums.length;
+    if(s === e) dp[i] = nums[i] + nums[s];
+    else if(s < e) {
+      if(e-s === 1) dp[i] = nums[i] + Math.max(nums[s], nums[e]);
+      else {
+        let acc = JSON.parse(JSON.stringify(nums.slice(s, e+1)));
+        for(let i=acc.length-3; i>=0; i--) {
+          acc[i] += Math.max.apply(null, acc.slice(i+2));
+        }
+        dp[i] = nums[i] + Math.max.apply(null, acc);
+      }
+    } else {
+      let acc = JSON.parse(JSON.stringify(nums.slice(s).concat(nums.slice(0,e+1))));
+      for(let i=acc.length-3; i>=0; i--) {
+        acc[i] += Math.max.apply(null, acc.slice(i+2));
+      }
+      dp[i] = nums[i] + Math.max.apply(null, acc);
+    }
+  }
+  return Math.max.apply(null, dp);
+};
