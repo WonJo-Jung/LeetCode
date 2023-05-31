b2815c7f0ce0b263b1e63deee058464eeb01026e
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-05-31 09:34:24 +0900

    Time: 59 ms (50.97%), Space: 42.1 MB (41.73%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  if(nums.length === 1) return nums[0];
+  nums[nums.length-2] = Math.max(nums[nums.length-2], nums[nums.length-1]);
+  for(let i=nums.length-3; i>=0; i--) {
+    nums[i] += Math.max.apply(null, nums.slice(i+2));
+  }
+  return Math.max.apply(null, nums);
+};
