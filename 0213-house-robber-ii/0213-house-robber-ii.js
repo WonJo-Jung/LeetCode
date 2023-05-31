9a52e0286160e440ba33c3f0a1ed02f331ced5ce
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-06-01 08:52:33 +0900

    Time: 88 ms (5.37%), Space: 48.1 MB (5.20%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.js
+++ 0213-house-robber-ii/0213-house-robber-ii.js
@@ -10,13 +10,10 @@ var rob = function(nums) {
     s = i+2, e = i-2;
     if(s > nums.length) s -= nums.length;
     if(e < 0) e += nums.length;
-    if(s === e) {
-      dp = Math.max(dp, nums[i] + nums[s]);
+    if(s === e || (s < e && e-s === 1)) {
+      if(s === e) dp = Math.max(dp, nums[i] + nums[s]);
+      else dp = Math.max(dp, nums[i] + Math.max(nums[s], nums[e]));
       continue;
-    }
-    else if(s < e && e-s === 1) {
-        dp = Math.max(dp, nums[i] + Math.max(nums[s], nums[e]));
-        continue;
     }
     if(s < e) acc = JSON.parse(JSON.stringify(nums.slice(s, e+1)));
     else acc = JSON.parse(JSON.stringify(nums.slice(s).concat(nums.slice(0,e+1))));
