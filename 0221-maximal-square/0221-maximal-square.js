4cd82b38e04868743650c8e0b6df246409c3ca2d
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-06-01 14:41:31 +0900

    Time: 92 ms (81.98%), Space: 49.5 MB (57.27%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.js
+++ 0221-maximal-square/0221-maximal-square.js
@@ -4,22 +4,35 @@
  */
 var maximalSquare = function(matrix) {
   let maxSize = 0, R = matrix.length, C = matrix[0].length;
-  for(let r=0; r<R; r++) {
-    for(let c=0; c<C; c++) {
-      if(matrix[r][c] === '1') {
-        let square = 1;
-        outer : for( ; r+square<R && c+square<C; square++) {
-          if(matrix[r+square][c+square] === '0') break;
-          for(let up=r+square-1; up>=r; up--) { // move up from diagonal position
-            if(matrix[up][c+square] === '0') break outer;
-          }
-          for(let left=c+square-1; left>=c; left--) { // move left from dianogal position
-            if(matrix[r+square][left] === '0') break outer;
-          }
-        }
-        maxSize = Math.max(maxSize, square * square);
+  let dp = [];
+  for(let i=0; i<R+1; i++) dp[i] = new Array(C+1).fill(0);
+  for(let i=1; i<=R; i++) {
+    for(let j=1; j<=C; j++) {
+      if(matrix[i-1][j-1] === '1') {
+        dp[i][j] = Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]) + 1;
+        maxSize = Math.max(maxSize, dp[i][j]);
       }
     }
   }
-  return maxSize;
+  return maxSize * maxSize;
+  /* Brute Force
+  // for(let r=0; r<R; r++) {
+  //   for(let c=0; c<C; c++) {
+  //     if(matrix[r][c] === '1') {
+  //       let square = 1;
+  //       outer : for( ; r+square<R && c+square<C; square++) {
+  //         if(matrix[r+square][c+square] === '0') break;
+  //         for(let up=r+square-1; up>=r; up--) { // move up from diagonal position
+  //           if(matrix[up][c+square] === '0') break outer;
+  //         }
+  //         for(let left=c+square-1; left>=c; left--) { // move left from dianogal position
+  //           if(matrix[r+square][left] === '0') break outer;
+  //         }
+  //       }
+  //       maxSize = Math.max(maxSize, square * square);
+  //     }
+  //   }
+  // }
+  // return maxSize;
+  */
 };
