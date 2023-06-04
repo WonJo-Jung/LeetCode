816cd5d86ff9c55a4c4b55bb1c508012264458ec
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-06-04 13:30:06 +0900

    Time: 161 ms (77.12%), Space: 48.5 MB (35.95%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.js
+++ 0279-perfect-squares/0279-perfect-squares.js
@@ -5,12 +5,9 @@
 var numSquares = function(n) {
   let dp = [0];
   for(let i=1; i<=n; i++) {
-    let square = Math.sqrt(i);
     let least = i; // only consists of 1
-    for(let j=Math.floor(square); j>=2; j--) {
-      let num = i-(j*j), count = 1;
-      count += dp[num];
-      least = Math.min(least, count);
+    for(let j=Math.floor(Math.sqrt(i)); j>=2; j--) {
+      least = Math.min(least, dp[i-(j*j)]+1);
     }
     dp[i] = least;
   }
