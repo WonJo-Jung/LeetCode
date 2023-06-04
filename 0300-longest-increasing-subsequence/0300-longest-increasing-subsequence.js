debb1489bd3e35725bd8a8be7c5e75d9215b9964
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-06-04 20:44:25 +0900

    Time: 152 ms (73.05%), Space: 44 MB (53.09%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  let n = nums.length;
+  let dp = new Array(n).fill(1);
+  for(let i=0; i<n; i++) {
+    for(let j=0; j<i; j++) {
+      if(nums[i] > nums[j] && dp[i] < dp[j] + 1) dp[i] = dp[j] + 1;
+    }
+  }
+  return Math.max.apply(null, dp);
+  
+  /* Heap OOM
+  let n=1;
+  let dp = new Array(nums.length).fill().map((_,idx) => idx);
+  while(dp.length > 0) { // length=n, strictly increasing subsequences
+    let arr = [];
+    for(let i=0; i<dp.length; i++) {
+      for(let j=dp[i]+1; j<nums.length; j++) {
+        if(nums[dp[i]] < nums[j]) arr[arr.length] = j;
+      }
+    }
+    dp = arr;
+    n++;
+  }
+  return n-1;
+  */
+};
