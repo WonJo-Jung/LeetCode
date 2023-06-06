6e0bb11f9b24cd73dc5ac87b998bc7128b5628c9
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-06-06 10:40:14 +0900

    Time: 79 ms (67.55%), Space: 47.3 MB (95.20%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+  for(let i=0, j=s.length-1; i<j; i++, j--) {
+    if(s[i] !== s[j]) {
+      let i1 = i+1, j1 = j, i2 = i, j2 = j-1;
+      while(i1 < j1 && s[i1] === s[j1]) {i1++; j1--;}
+      while(i2 < j2 && s[i2] === s[j2]) {i2++; j2--;}
+      return i1>=j1 || i2>=j2;
+    }
+  }
+  return true;
+};
