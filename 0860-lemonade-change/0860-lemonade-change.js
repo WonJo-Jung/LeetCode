0a5eb237d11140c78a9908f36b027f2ec4af857a
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-06-06 11:02:32 +0900

    Time: 81 ms (72.65%), Space: 50.6 MB (54.29%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.js
+++ 0860-lemonade-change/0860-lemonade-change.js
@@ -3,20 +3,19 @@
  * @return {boolean}
  */
 var lemonadeChange = function(bills) {
-  let map = new Map();
-  map.set(5, 0); map.set(10, 0);
+  let five = 0, ten = 0;
   for(let i=0; i<bills.length; i++) {
-    if(bills[i] === 5) map.set(5, map.get(5)+1); // change 0
+    if(bills[i] === 5) five++; // change 0
     else if(bills[i] === 10) { // change 5
-      if(map.get(5) < 1) return false;
-      map.set(5, map.get(5)-1);
-      map.set(10, map.get(10)+1);
+      if(five < 1) return false;
+      five--;
+      ten++;
     }
     else if(bills[i] === 20) { // change 10+5 or 5+5+5
-      if(map.get(10) >= 1 && map.get(5) >= 1) {
-        map.set(10, map.get(10)-1);
-        map.set(5, map.get(5)-1);
-      } else if(map.get(5) >= 3) map.set(5, map.get(5)-3);
+      if(ten >= 1 && five >= 1) {
+        ten--;
+        five--;
+      } else if(five >= 3) five -= 3;
       else return false;
     }
   }
