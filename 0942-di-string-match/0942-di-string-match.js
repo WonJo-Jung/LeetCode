59466ebf7d2fd7e31fadf8cec2c859a4add72b29
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-06-06 15:10:05 +0900

    Time: 81 ms (56.86%), Space: 44.7 MB (69.12%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.js
+++ 0942-di-string-match/0942-di-string-match.js
@@ -3,9 +3,9 @@
  * @return {number[]}
  */
 var diStringMatch = function(s) {
-  let arr = [], index = 0;
+  let arr = [];
   for(let i=0, j=s.length; i<=j; ) {
-    if(s[index++] === "I" || i === j) arr[arr.length] = i++;
+    if(s[arr.length] === "I" || i === j) arr[arr.length] = i++;
     else arr[arr.length] = j--;
   }
   return arr;
