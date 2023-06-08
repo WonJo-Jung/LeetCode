a37af376b2c7a1f9c896d4414ecc00c30c3fa565
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-06-08 16:47:51 +0900

    Time: 75 ms (20.64%), Space: 44.1 MB (61.08%) - LeetHub

--- 0179-largest-number/0179-largest-number.js
+++ 0179-largest-number/0179-largest-number.js
@@ -3,17 +3,21 @@
  * @return {string}
  */
 var largestNumber = function(nums) {
-  let str = [nums[0]];
-  for(let i=1; i<nums.length; i++) {
-    let j=0;
-    for(; j<str.length; j++) {
-      if(Number(""+nums[i]+str[j]) > Number(""+str[j]+nums[i])) {
-        str = str.slice(0, j).concat(nums[i]).concat(str.slice(j));
-        break;
-      }
-    }
-    if(j === str.length) str[str.length] = nums[i];
-  }
-  if(str[0] === 0) return "0";
-  return str.join("");
+  nums = nums.sort((a,b) => (""+b+a) - (""+a+b));
+  if(nums[0] === 0) return "0";
+  else return nums.join("");
+  
+  // let str = [nums[0]];
+  // for(let i=1; i<nums.length; i++) {
+  //   let j=0;
+  //   for(; j<str.length; j++) {
+  //     if(Number(""+nums[i]+str[j]) > Number(""+str[j]+nums[i])) {
+  //       str = str.slice(0, j).concat(nums[i]).concat(str.slice(j));
+  //       break;
+  //     }
+  //   }
+  //   if(j === str.length) str[str.length] = nums[i];
+  // }
+  // if(str[0] === 0) return "0";
+  // return str.join("");
 };
