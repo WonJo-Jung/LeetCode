0d5c2e56534e504893d14a19a1f3aa7c11751c15
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-06-26 11:20:51 +0900

    Time: 81 ms (10.58%), Space: 45.1 MB (13.32%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -10,6 +10,42 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+/** Use Stack
+*/
+var isSymmetric = function(root) {
+  let stack = [], left, right;
+  if(root.left !== null) {
+    if(root.right === null) return false;
+    stack[stack.length] = root.left;
+    stack[stack.length] = root.right;
+  } else if(root.right !== null) {
+    return false;
+  }
+  
+  while(stack.length > 0) {
+    if(stack.length%2 !== 0) return false;
+    right = stack.pop();
+    left = stack.pop();
+    if(right.val !== left.val) return false;
+    
+    if(left.left !== null) {
+      if(right.right === null) return false;
+      stack[stack.length] = left.left;
+      stack[stack.length] = right.right;
+    } else if(right.right !== null) return false;
+    
+    if(left.right !== null) {
+      if(right.left === null) return false;
+      stack[stack.length] = left.right;
+      stack[stack.length] = right.left;
+    } else if(right.left !== null) return false;
+  }
+  
+  return true;
+};
+
+
+/** Use Recurse
 var isSymmetric = function(root) {
   return isSymmetricHelp(root.left, root.right);
 };
@@ -19,3 +55,4 @@ let isSymmetricHelp = function(left, right) {
   if(left.val !== right.val) return false;
   return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
 }
+*/
