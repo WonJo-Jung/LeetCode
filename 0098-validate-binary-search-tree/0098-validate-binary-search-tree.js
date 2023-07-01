51ae14e8dfcbdb4432cc9748c401aa4889be0833
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-07-01 12:45:57 +0900

    Time: 72 ms (54.32%), Space: 47.1 MB (14.65%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -10,7 +10,21 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {  
+var isValidBST = function(root) {
+  let stack = [], prev = null;
+  while(root !== null || stack.length > 0) {
+    while(root !== null) {
+      stack.push(root);
+      root = root.left;
+    }
+    root = stack.pop();
+    if(prev !== null && prev.val >= root.val) return false;
+    prev = root;
+    root = root.right;
+  }
+  return true;
+  
+  /*
   function subBST(root) {
     if(root === null) return [];
     if(root.left === null && root.right === null) return [root.val];
@@ -20,4 +34,5 @@ var isValidBST = function(root) {
   let arr = subBST(root.left).concat([root.val], subBST(root.right));
   for(let i=0; i<arr.length-1; i++) if(arr[i] >= arr[i+1]) return false;
   return true;
+  */
 };
