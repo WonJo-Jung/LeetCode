2d55a1591a6e27e47896d0f4f52263848ed61f0e
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-07-02 11:22:57 +0900

    Time: 88 ms (16.24%), Space: 48.6 MB (81.92%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.js
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.js
@@ -14,14 +14,24 @@
  */
 var connect = function(root) {
   if(!root) return root;
-  let queue = [root];
-  while(queue.length) {
-    let count = queue.length;
-    while(count--) {
-      let node = queue.shift();
-      if(count) node.next = queue[0];
-      if(node.left && node.right) queue.push(node.left, node.right);
+  let start = root;
+  while(start.left && start.right) {
+    let curr = start;
+    while(curr) {
+      curr.left.next = curr.right;
+      if(curr.next) curr.right.next = curr.next.left;
+      curr = curr.next;
     }
+    start = start.left;
   }
+  // let queue = [root];
+  // while(queue.length) {
+  //   let count = queue.length;
+  //   while(count--) {
+  //     let node = queue.shift();
+  //     if(count) node.next = queue[0];
+  //     if(node.left && node.right) queue.push(node.left, node.right);
+  //   }
+  // }
   return root;
 };
