c0b626c4a0f68ee622ed93e3b4bba0161095a1d3
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-07-03 09:25:49 +0900

    Time: 95 ms (35.59%), Space: 46.8 MB (58.38%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.js
+++ 0130-surrounded-regions/0130-surrounded-regions.js
@@ -4,6 +4,44 @@
  */
 var solve = function(board) {
   let m = board.length, n = board[0].length;
+  let dy = [-1,1,0,0];
+  let dx = [0,0,-1,1];
+  let border_path = [];
+  
+  for(let i=0; i<m; i++) {
+    for(let j=0; j<n; j++) {
+      if(i === 0 || i === m-1 || j === 0 || j === n-1) {
+        if(board[i][j] === "O") {
+          let stack = [[i, j]], path = [];
+          while(stack.length) {
+            let pos = stack.pop();
+            path.push(pos);
+            board[pos[0]][pos[1]] = "#";
+            for(let k=0; k<4; k++) {
+              let y = pos[0] + dy[k], x = pos[1] + dx[k];
+              if(y >= 0 && y < m && x >= 0 && x < n && board[y][x] === "O") stack.push([y, x]);
+            }
+          }
+          border_path.push(path);
+        }
+      }
+    }
+  }
+  
+  for(let i=1; i<m-1; i++) {
+    for(let j=1; j<n-1; j++) {
+      if(board[i][j] === "O") board[i][j] = "X";
+    }
+  }
+  
+  for(let i=0; i<border_path.length; i++) {
+    let path = border_path[i];
+    for(let j=0; j<path.length; j++) {
+      board[path[j][0]][path[j][1]] = "O";
+    }
+  }
+  
+  /*
   if(m <= 2 || n <= 2) return;
   
   let visited = [];
@@ -15,9 +53,8 @@ var solve = function(board) {
   for(let i=0; i<m; i++) { // y-axis
     for(let j=0; j<n; j++) { // x-axis
       if(board[i][j] === "O" && !visited[i][j]) {
-        if(i === 0 || i === m-1 || j === 0 || j === n-1) continue;
         let path = [], stack = [[i,j]]; // y,x
-        let flippable = true;
+        let flippable = (i === 0 || i === m-1 || j === 0 || j === n-1) ? false : true;
         while(stack.length) {
           let pos = stack.pop();
           visited[pos[0]][pos[1]] = true;
@@ -37,4 +74,5 @@ var solve = function(board) {
       }
     }
   }
+  */
 };
