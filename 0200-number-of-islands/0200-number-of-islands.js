ca19e64a581c68cb19045a24aa29d02ba873bcff
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-07-07 09:34:17 +0900

    Time: 109 ms (27.00%), Space: 52.9 MB (22.50%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,7 +3,27 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-  let islands = [], visited = [], m = grid.length, n = grid[0].length;
+  let islands = [], visited = [], m = grid.length, n = grid[0].length, count = 0;
+  for(let i=0; i<m; i++) visited[i] = new Array(n).fill(false);
+  for(let i=0; i<m; i++) {
+    for(let j=0; j<n; j++) {
+      if(grid[i][j] === "1" && !visited[i][j]) {
+        count++;
+        let stack = [[i,j]];
+        while(stack.length) {
+          let [y, x] = stack.pop();
+          visited[y][x] = true;
+          if(y-1 >= 0 && grid[y-1][x] === "1" && !visited[y-1][x]) stack.push([y-1, x]);
+          if(y+1 < m && grid[y+1][x] === "1" && !visited[y+1][x]) stack.push([y+1, x]);
+          if(x-1 >= 0 && grid[y][x-1] === "1" && !visited[y][x-1]) stack.push([y, x-1]);
+          if(x+1 < n && grid[y][x+1] === "1" && !visited[y][x+1]) stack.push([y, x+1]);
+        }
+      }
+    }
+  }
+  return count;
+  
+  /*
   for(let i=0; i<m+1; i++) visited[i] = new Array(n+1).fill(-1);
   
   for(let i=0; i<m; i++) {
@@ -39,4 +59,5 @@ var numIslands = function(grid) {
     }
   }
   return islands.filter(island => island.length).length;
+  */
 };
