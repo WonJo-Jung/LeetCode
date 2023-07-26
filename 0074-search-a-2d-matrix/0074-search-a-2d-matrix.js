3e2d32c66c8ffa78169757e1c6cb076e679146f9
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-07-26 16:45:23 +0900

    Time: 58 ms (65.83%), Space: 41.6 MB (89.37%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+  let m = matrix.length, n = matrix[0].length;
+  let row = 0;
+  while(row < m) {
+    if(target <= matrix[row][n-1]) {
+      let left = 0, right = n - 1, mid;
+      while(left <= right) {
+        mid = Math.floor((left+right)/2);
+        if(matrix[row][mid] === target) return true;
+        else if(matrix[row][mid] < target) left = mid + 1;
+        else right = mid - 1;
+      }
+    }
+    row++;
+  }
+  return false;
+};
