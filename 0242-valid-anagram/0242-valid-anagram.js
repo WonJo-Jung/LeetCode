2660db190f9b1db25049e13458836e755cc03c6e
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-09-10 17:06:37 +0900

    Time: 87 ms (30.65%), Space: 48.6 MB (15.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,8 +4,5 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  let alphabet = new Array(128).fill(0);
-  for(let i=0; i<s.length; i++) alphabet[s.charCodeAt(i)]++;
-  for(let i=0; i<t.length; i++) alphabet[t.charCodeAt(i)]--;
-  return alphabet.every(alpha => alpha === 0);
+  return s.split("").sort((a,b) => a.charCodeAt() - b.charCodeAt()).join("") === t.split("").sort((a,b) => a.charCodeAt() - b.charCodeAt()).join("");
 };
