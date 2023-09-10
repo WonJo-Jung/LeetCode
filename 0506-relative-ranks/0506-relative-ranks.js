ff80f22b9b9247818e252bde22996460e6f17d9d
Skipper <35194799+WonJo-Jung@users.noreply.github.com>
2023-09-10 22:50:28 +0900

    Time: 99 ms (31.18%), Space: 45 MB (53.76%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.js
+++ 0506-relative-ranks/0506-relative-ranks.js
@@ -4,11 +4,9 @@
  */
 var findRelativeRanks = function(score) {
   let ranks = JSON.parse(JSON.stringify(score)).sort((a,b) => b-a);
-  let map = new Map();
-  for(let i=0; i<ranks.length; i++) map.set(ranks[i], i);
   let answer = [];
   for(let i=0; i<score.length; i++) {
-    let rank = map.get(score[i]);
+    let rank = ranks.indexOf(score[i]);
     if(rank == 0) answer.push("Gold Medal");
     else if(rank == 1) answer.push("Silver Medal");
     else if(rank == 2) answer.push("Bronze Medal");
